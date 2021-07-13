b163764512504ccb84d0500a23bbd89ff4746c39
Joshua Renfro <jrdarknight@sbcglobal.net>
2021-07-13 11:10:47 -0500

    Added comments and to test webhook.

--- RomaniaProblem/main.cpp
+++ RomaniaProblem/main.cpp
@@ -52,12 +52,18 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		//game->run(window);	//main loop of program
+		// Input
+		// -----
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 			glfwSetWindowShouldClose(window, true);
 
+		// Render
+		// ------
 		glClearColor(0.1f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
